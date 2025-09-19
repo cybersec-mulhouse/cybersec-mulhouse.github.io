5233a09659088fea01cff6df3dfd0f684edf84ed
Alex Collignon <alexandre@collign.net>
2025-09-19 07:56:02 +0200

    feat: add registration form

--- cybsecmulhouse/pages/register.vue
+++ cybsecmulhouse/pages/register.vue
@@ -35,14 +35,10 @@ import type { FormSubmitEvent } from '#ui/types'
       </template>
       <template #footer>
       <div class="flex md:flex-row  flex-col text-white">
-        <!-- <a href="https://discord.com/invite/PvC8bWH3mv?event=1250008749712740403"> <img src="~/assets/img/qr-code.png" alt="" class="-z-10 h-full w-72 object-cover object-right md:object-center"/></a> -->
-        <!-- <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeBtrPzvMBsZ1tMQzg9uvBXr2Y8GthZzNuRuKPkZLq20hmeKQ/viewform?embedded=true"  width="640" height="800" frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>
-        
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe9Z3X4e2lnvwYYnwG-dp66bPdf03quO8dFTP8hBMk-yKaF-Q/viewform?embedded=true"  width="640" height="800" frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>-->
-      <h2 class="text-2xl">{{ $t('registration_not_open_m') }}</h2>  
+<!--        <a href="https://discord.com/invite/PvC8bWH3mv?event=1250008749712740403"> <img src="~/assets/img/qr-code.png" alt="" class="-z-10 h-full w-72 object-cover object-right md:object-center"/></a> -->
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSf_ZYgXj2_Y_zHcYV8tQHBojRJIcwZrj084Tf6MpH7S95xiIA/viewform?embedded=true"  width="720" height="1000" frameborder="0" marginheight="0" marginwidth="0">Chargement…</iframe>
       </div>  
       <!-- 1f5Kp1b1IcMfBT0X1fBmpJI4S7kKUNoK6lOoGNee26s4 -->
-      
       </template>
     </PageCard>
   </template>
