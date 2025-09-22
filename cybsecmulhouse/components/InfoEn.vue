d644b8172a83f94bd6fe4c1d7ee57679ea70d88f
Alex Collignon <alexandre@collign.net>
2025-09-22 19:37:53 +0200

    fix: english schedule

--- cybsecmulhouse/components/InfoEn.vue
+++ cybsecmulhouse/components/InfoEn.vue
@@ -3,17 +3,11 @@
     class="text-white text-2xl font-light justify-center mt-10 flex flex-row"
   >
     <ul class="list-disc pl-10">
-      <template>
-        <div class="text-white text-2xl font-light justify-center mt-10 flex flex-col">
-          <ul class="list-disc pl-10">
-            <!--      <li>Event Presentation at 4:00 PM</li> -->
-            <li>Team Reception & Welcome at 9:00 AM</li>
-            <li>CTF Start at 10:00 AM</li>
-            <li>Score Closing at 5:00 PM</li>
-            <li>Awards Ceremony & Prizes at 6:00 PM</li>
-          </ul>
-        </div>
-      </template>
+      <!--      <li>Event Presentation at 4:00 PM</li> -->
+      <li>Team Reception & Welcome at 9:00 AM</li>
+      <li>CTF Start at 10:00 AM</li>
+      <li>Score Closing at 5:00 PM</li>
+      <li>Awards Ceremony & Prizes at 6:00 PM</li>
     </ul>
   </div>
 </template>
