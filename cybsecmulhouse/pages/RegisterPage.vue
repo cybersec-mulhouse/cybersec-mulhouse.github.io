689c7c589e4b35813d4e6fbc0ebec8fbd06a7d55
Alex Collignon <alexandre@collign.net>
2025-11-25 09:58:36 +0100

    feat: add discord server

--- cybsecmulhouse/pages/RegisterPage.vue
+++ cybsecmulhouse/pages/RegisterPage.vue
@@ -9,9 +9,14 @@
     </template>
     <template #body />
     <template #footer>
-      <div class="flex md:flex-row  flex-col text-white">
+          <div class="flex flex-col-reverse col-span-4 text-white">
+          Server Discord
+        <a href="https://discord.com/invite/PvC8bWH3mv?event=1250008749712740403"> <img src="~/assets/img/qr-code.png" alt="" class="-z-10 h-full w-72 object-cover object-right md:object-center"/></a>
+        </div>
+<!--      <div class="flex md:flex-row  flex-col text-white">
         {{ $t('registration_closed_m') }}
       </div>
-    </template>
+        -->
+</template>
   </PageCard>
 </template>
